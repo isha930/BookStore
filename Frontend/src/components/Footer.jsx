6ad6bfc1dec91dec668f52561a87ddc7e35d1550
Isha <ishadurge7@gmail.com>
2025-03-22 23:13:15 +0530

    Frontend

--- Frontend/src/components/Footer.jsx
+++ Frontend/src/components/Footer.jsx
@@ -47,7 +47,7 @@ function Footer() {
           </div>
         </nav>
         <aside>
-          <p>Copyright © 2024 - All right reserved !</p>
+          <p>Copyright © 2025 - All right reserved !</p>
         </aside>
       </footer>
     </div>
